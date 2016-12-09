0dc412abcc67e9646d181dab4a7d9fd61c9d0786
Nick Kezhaya <nick@whitepaperclip.com>
2016-12-09 10:26:53 -0600

    Preparing to publish to hex

--- mix.exs
+++ mix.exs
@@ -7,25 +7,33 @@ defmodule Shippex.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    """
+    Shippex is an abstraction of commonly used features in shipping with various
+    carriers. It provides a (hopefully) pleasant API to work with carrier-
+    provided web interfaces for fetching rates and printing shipping labels.
+    """
+  end
+
+  defp package do
+    [
+      name: :shippex,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Nick Kezhaya"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/whitepaperclip/shippex"}
+    ]
+  end
+
   defp deps do
     [
       {:httpoison, ">= 0.0.0"},

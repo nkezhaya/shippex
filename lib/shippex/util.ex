19cafd4667a566b04d890a399610f9e753bd412b
Nick Kezhaya <nick@whitepaperclip.com>
2016-12-12 12:14:44 -0600

    Use byte_size() for determining state string length.

--- lib/shippex/util.ex
+++ lib/shippex/util.ex
@@ -1,76 +1,78 @@
 defmodule Shippex.Util do
-    @states [
-      ["Alabama", "AL"],
-      ["Alaska", "AK"],
-      ["Arizona", "AZ"],
-      ["Arkansas", "AR"],
-      ["California", "CA"],
-      ["Colorado", "CO"],
-      ["Connecticut", "CT"],
-      ["Delaware", "DE"],
-      ["District of Columbia", "DC"],
-      ["Florida", "FL"],
-      ["Georgia", "GA"],
-      ["Hawaii", "HI"],
-      ["Idaho", "ID"],
-      ["Illinois", "IL"],
-      ["Indiana", "IN"],
-      ["Iowa", "IA"],
-      ["Kansas", "KS"],
-      ["Kentucky", "KY"],
-      ["Louisiana", "LA"],
-      ["Maine", "ME"],
-      ["Maryland", "MD"],
-      ["Massachusetts", "MA"],
-      ["Michigan", "MI"],
-      ["Minnesota", "MN"],
-      ["Mississippi", "MS"],
-      ["Missouri", "MO"],
-      ["Montana", "MT"],
-      ["Nebraska", "NE"],
-      ["Nevada", "NV"],
-      ["New Hampshire", "NH"],
-      ["New Jersey", "NJ"],
-      ["New Mexico", "NM"],
-      ["New York", "NY"],
-      ["North Carolina", "NC"],
-      ["North Dakota", "ND"],
-      ["Ohio", "OH"],
-      ["Oklahoma", "OK"],
-      ["Oregon", "OR"],
-      ["Pennsylvania", "PA"],
-      ["Rhode Island", "RI"],
-      ["South Carolina", "SC"],
-      ["South Dakota", "SD"],
-      ["Tennessee", "TN"],
-      ["Texas", "TX"],
-      ["Utah", "UT"],
-      ["Vermont", "VT"],
-      ["Virginia", "VA"],
-      ["Washington", "WA"],
-      ["West Virginia", "WV"],
-      ["Wisconsin", "WI"],
-      ["Wyoming", "WY"]
-    ]
+  @states [
+    ["Alabama", "AL"],
+    ["Alaska", "AK"],
+    ["Arizona", "AZ"],
+    ["Arkansas", "AR"],
+    ["California", "CA"],
+    ["Colorado", "CO"],
+    ["Connecticut", "CT"],
+    ["Delaware", "DE"],
+    ["District of Columbia", "DC"],
+    ["Florida", "FL"],
+    ["Georgia", "GA"],
+    ["Hawaii", "HI"],
+    ["Idaho", "ID"],
+    ["Illinois", "IL"],
+    ["Indiana", "IN"],
+    ["Iowa", "IA"],
+    ["Kansas", "KS"],
+    ["Kentucky", "KY"],
+    ["Louisiana", "LA"],
+    ["Maine", "ME"],
+    ["Maryland", "MD"],
+    ["Massachusetts", "MA"],
+    ["Michigan", "MI"],
+    ["Minnesota", "MN"],
+    ["Mississippi", "MS"],
+    ["Missouri", "MO"],
+    ["Montana", "MT"],
+    ["Nebraska", "NE"],
+    ["Nevada", "NV"],
+    ["New Hampshire", "NH"],
+    ["New Jersey", "NJ"],
+    ["New Mexico", "NM"],
+    ["New York", "NY"],
+    ["North Carolina", "NC"],
+    ["North Dakota", "ND"],
+    ["Ohio", "OH"],
+    ["Oklahoma", "OK"],
+    ["Oregon", "OR"],
+    ["Pennsylvania", "PA"],
+    ["Rhode Island", "RI"],
+    ["South Carolina", "SC"],
+    ["South Dakota", "SD"],
+    ["Tennessee", "TN"],
+    ["Texas", "TX"],
+    ["Utah", "UT"],
+    ["Vermont", "VT"],
+    ["Virginia", "VA"],
+    ["Washington", "WA"],
+    ["West Virginia", "WV"],
+    ["Wisconsin", "WI"],
+    ["Wyoming", "WY"]
+  ]
 
-    def full_state_to_abbreviation(state) when is_bitstring(state) and length(state) == 2 do
-      state
-    end
+  def full_state_to_abbreviation(state) when is_bitstring(state) and byte_size(state) == 2 do
+    state
+  end
 
-    def full_state_to_abbreviation(state) when is_bitstring(state) do
-      result = Enum.find @states, fn (s) ->
-        [full, abbr] = s
+  def full_state_to_abbreviation(state) when is_bitstring(state) do
+    state = String.downcase(state)
 
-        if String.downcase(full) == String.downcase(state) do
-          abbr
-        else
-          false
-        end
-      end
+    result = Enum.find @states, fn (s) ->
+      [full, abbr] = s
 
-      case result do
-        [_full, abbr] -> abbr
-        _ -> nil
+      if state == String.downcase(full) do
+        abbr
+      else
+        false
       end
     end
+
+    case result do
+      [_full, abbr] -> abbr
+      _ -> nil
+    end
+  end
 end

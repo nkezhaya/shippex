aafd071b81c0865bd8e1ee90edad84a23fc2f396
Nick Kezhaya <nick@whitepaperclip.com>
2017-07-19 11:26:41 -0500

    No need to validate address in USPS rate test

--- test/usps/rate_test.exs
+++ test/usps/rate_test.exs
@@ -7,30 +7,12 @@ defmodule Shippex.USPS.RateTest do
     [shipment: Helper.valid_shipment]
   end
 
-  describe "addresses" do
-    test "validate address" do
-      valid_address = Shippex.Address.address(%{
-        "name" => "Earl G",
-        "phone" => "123-456-7890",
-        "address" => "404 S Figueroa St",
-        "address_line_2" => "Suite 101",
-        "city" => "Los Angeles",
-        "state" => "CA",
-        "zip" => "90071"
-      })
-
-      Shippex.Carrier.USPS.validate_address(valid_address)
-    end
-  end
-
-  describe "rates" do
-    test "rates generated", %{shipment: shipment} do
-      package = %{shipment.package | container: :variable}
-      shipment = %{shipment | package: package}
-      rates = Shippex.Carrier.USPS.fetch_rate(shipment, "priority")
-      assert is_list(rates) and length(rates) > 0
-      rates = Shippex.Carrier.USPS.fetch_rate(shipment, "all")
-      assert is_list(rates) and length(rates) > 0
-    end
+  test "rates generated", %{shipment: shipment} do
+    package = %{shipment.package | container: :variable}
+    shipment = %{shipment | package: package}
+    rates = Shippex.Carrier.USPS.fetch_rate(shipment, "priority")
+    assert is_list(rates) and length(rates) > 0
+    rates = Shippex.Carrier.USPS.fetch_rate(shipment, "all")
+    assert is_list(rates) and length(rates) > 0
   end
 end

3b93e84fe24b8a041d1384ab2cee1cfc6fd7335f
Nick Kezhaya <nick@whitepaperclip.com>
2018-02-23 15:31:14 -0600

    v0.6.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shippex.Mixfile do
   def project do
     [
       app: :shippex,
-      version: "0.6.5",
+      version: "0.6.6",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

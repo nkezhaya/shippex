5d14f2b9d2f9da4011d9042e518621d6e7969a34
Nick Kezhaya <nick@whitepaperclip.com>
2018-06-07 19:59:28 -0500

    US is a valid destination

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shippex.Mixfile do
   def project do
     [
       app: :shippex,
-      version: "0.6.9",
+      version: "0.6.10",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
